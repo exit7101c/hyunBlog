80d528c9c1f16d430b25314de3870c1bed3e5d5c
hyun_todolist <exit7101c@gmail.com>
2024-03-08 22:55:56 +0900

    cros 추가.

--- src/main/java/com/hyun/apiblog/HyunApiBlogApplication.java
+++ src/main/java/com/hyun/apiblog/HyunApiBlogApplication.java
@@ -2,6 +2,9 @@ package com.hyun.apiblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HyunApiBlogApplication {
@@ -9,5 +12,14 @@ public class HyunApiBlogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HyunApiBlogApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*");
+			}
+		};
+	}
 }
